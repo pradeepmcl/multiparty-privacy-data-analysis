882f9f55ae452c5692ee37d0f3e787ee81ad8153
pradeepmcl@gmail.com <pradeepmcl@gmail.com>
2015-11-24 18:06:16 -0500

    Intermediate commit; working on a TF-IDF builder

--- src/main/java/edu/ncsu/mas/platys/multiparty_privacy/nlp/TfIdfBuilder.java
+++ src/main/java/edu/ncsu/mas/platys/multiparty_privacy/nlp/TfIdfBuilder.java
@@ -9,7 +9,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -27,7 +29,7 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
-public class JustificationAnalyser implements AutoCloseable {
+public class TfIdfBuilder implements AutoCloseable {
 
   private final Properties mJdbcProps = new Properties();
 
@@ -35,7 +37,9 @@ public class JustificationAnalyser implements AutoCloseable {
 
   private StanfordCoreNLP mNlpPipeline;
 
-  public JustificationAnalyser() throws SQLException, FileNotFoundException, IOException,
+  private Map<Integer, String> idToJustificationMap = new HashMap<Integer, String>();
+
+  public TfIdfBuilder() throws SQLException, FileNotFoundException, IOException,
       ClassNotFoundException {
     try (InputStream inStream = new FileInputStream("src/main/resources/application.properties")) {
       // Load properties file
@@ -59,61 +63,47 @@ public class JustificationAnalyser implements AutoCloseable {
     StanfordCoreNLP.clearAnnotatorPool();
   }
 
-  public void getSentiment(String text) {
+  private void readJustifications() throws SQLException {
+    try (Statement stmt = mConn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id, case1_policy_justification"
+            + " FROM turker_picturesurvey_response")) {
+      while (rs.next()) {
+        String case1PolicyJustification = rs.getString("case1_policy_justification");
+        
+        // case1PolicyJustification.contains("Dummy")
+        if (case1PolicyJustification.trim().length() < 10) {
+          continue;
+        }
+
+        idToJustificationMap.put(rs.getInt("id"), case1PolicyJustification);
+      }
+    }
+  }
+  
+  public void getPosDistribution() {
+    
+  }
+  
+  
+  public void getWords(String text) {
+    List<String> words = new ArrayList<String>();
+    
     Annotation annotation = new Annotation(text);
     mNlpPipeline.annotate(annotation);
 
     for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
       for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
         String word = token.get(TextAnnotation.class);
-        String pos = token.get(PartOfSpeechAnnotation.class);
-        String ne = token.get(NamedEntityTagAnnotation.class);
-        System.out.println(word + "/" + pos + "/" + ne);
+        // TODO: Can exclude some words here, e.g., is, to, because, etc.
+        words.add(word);
       }
-
-      Tree tree = sentence.get(TreeAnnotation.class);
-      System.out.println(tree);
-      SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-      System.out.println(dependencies);
     }
   }
 
-  public Map<String, Integer> getPosDistribution() throws SQLException {
-    Map<String, Integer> posCounter = new HashMap<String, Integer>();
-
-    try (Statement stmt = mConn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT case1_policy_justification"
-            + " FROM turker_picturesurvey_response")) {
-      while (rs.next()) {
-        String case1PolicyJustification = rs.getString("case1_policy_justification");
-        if (case1PolicyJustification.trim().length() < 10) {
-          continue;
-        }
-
-        Annotation annotation = new Annotation(case1PolicyJustification);
-        mNlpPipeline.annotate(annotation);
-
-        for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-          for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-            String pos = token.get(PartOfSpeechAnnotation.class);
-            Integer count = posCounter.get(pos);
-            if (count == null) {
-              posCounter.put(pos, 1);
-            } else {
-              posCounter.put(pos, count + 1);
-            }
-          }
-        }
-      }
-    }
-
-    return posCounter;
-  }
-
-  public static void main(String[] args) throws SQLException, Exception {
-    try (JustificationAnalyser justificationAnalyser = new JustificationAnalyser()) {
-      Map<String, Integer> posCounter = justificationAnalyser.getPosDistribution();
-      System.out.println(posCounter);
+  public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException,
+      IOException, Exception {
+    try (TfIdfBuilder justificationAnalyser = new TfIdfBuilder()) {
+      justificationAnalyser.readJustifications();
     }
   }
 }
