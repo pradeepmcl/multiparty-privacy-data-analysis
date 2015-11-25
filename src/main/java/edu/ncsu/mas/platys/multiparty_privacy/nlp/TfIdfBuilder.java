205612f2d9830256cf6c7265a3ec57fcaac499b3
pradeepmcl@gmail.com <pradeepmcl@gmail.com>
2015-11-25 14:20:12 -0500

    A working version of the TF-IDF builder

--- src/main/java/edu/ncsu/mas/platys/multiparty_privacy/nlp/TfIdfBuilder.java
+++ src/main/java/edu/ncsu/mas/platys/multiparty_privacy/nlp/TfIdfBuilder.java
@@ -4,32 +4,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 public class TfIdfBuilder implements AutoCloseable {
+
+  public enum WordFilterType {
+    NONE, POS
+  };
 
   private final Properties mJdbcProps = new Properties();
 
@@ -38,6 +41,15 @@ public class TfIdfBuilder implements AutoCloseable {
   private StanfordCoreNLP mNlpPipeline;
 
   private Map<Integer, String> idToJustificationMap = new HashMap<Integer, String>();
+
+  // For TF, count terms in each document
+  private Table<Integer, String, Integer> docTermCounts = HashBasedTable.create();
+
+  // For IDF, count number of docs for each term
+  private Map<String, Integer> termDocCounts = new HashMap<String, Integer>();
+
+  // TF-IDF table
+  private Table<Integer, String, Double> tfIdf = HashBasedTable.create();
 
   public TfIdfBuilder() throws SQLException, FileNotFoundException, IOException,
       ClassNotFoundException {
@@ -69,9 +81,10 @@ public class TfIdfBuilder implements AutoCloseable {
             + " FROM turker_picturesurvey_response")) {
       while (rs.next()) {
         String case1PolicyJustification = rs.getString("case1_policy_justification");
-        
-        // case1PolicyJustification.contains("Dummy")
-        if (case1PolicyJustification.trim().length() < 10) {
+
+        if (case1PolicyJustification.startsWith("Dummy")
+            || case1PolicyJustification.startsWith("dummy")
+            || case1PolicyJustification.trim().length() < 10) {
           continue;
         }
 
@@ -79,23 +92,65 @@ public class TfIdfBuilder implements AutoCloseable {
       }
     }
   }
-  
-  public void getPosDistribution() {
-    
-  }
-  
-  
-  public void getWords(String text) {
-    List<String> words = new ArrayList<String>();
-    
+
+  private void updateTermAndDocumentCounts(Integer id, String text, WordFilterType WordFilterType) {
+    Set<String> terms = new HashSet<String>();
+
     Annotation annotation = new Annotation(text);
     mNlpPipeline.annotate(annotation);
 
     for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
       for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-        String word = token.get(TextAnnotation.class);
-        // TODO: Can exclude some words here, e.g., is, to, because, etc.
-        words.add(word);
+
+        String term = token.get(TextAnnotation.class);
+
+        switch (WordFilterType) {
+        case NONE:
+          break;
+        case POS:
+          // TODO: Exclude some words, e.g., is, to, because, etc.
+          break;
+        default:
+          continue;
+        }
+
+        term = term.toLowerCase();
+        
+        terms.add(term);
+
+        Integer termCount = docTermCounts.get(id, term);
+        if (termCount == null) {
+          docTermCounts.put(id, term, 1);
+        } else {
+          docTermCounts.put(id, term, termCount + 1);
+        }
+      }
+    }
+
+    for (String term : terms) {
+      Integer docCount = termDocCounts.get(term);
+      if (docCount == null) {
+        termDocCounts.put(term, 1);
+      } else {
+        termDocCounts.put(term, docCount + 1);
+      }
+    }
+  }
+
+  public void buildTfIdf() {
+    for (Integer id : idToJustificationMap.keySet()) {
+      String justification = idToJustificationMap.get(id);
+      updateTermAndDocumentCounts(id, justification, WordFilterType.NONE);
+    }
+
+    // Change term counts to term frequencies
+    for (Integer docId : docTermCounts.rowKeySet()) {
+      Map<String, Integer> termCounts = docTermCounts.row(docId);
+      Integer maxCount = Collections.max(termCounts.values());
+      for (String term : termCounts.keySet()) {
+        double tF = 0.5 + (0.5 * termCounts.get(term) / maxCount);
+        double iDF = Math.log((double) idToJustificationMap.size() / termDocCounts.get(term));
+        tfIdf.put(docId, term, tF * iDF);
       }
     }
   }
@@ -104,6 +159,16 @@ public class TfIdfBuilder implements AutoCloseable {
       IOException, Exception {
     try (TfIdfBuilder justificationAnalyser = new TfIdfBuilder()) {
       justificationAnalyser.readJustifications();
+      justificationAnalyser.buildTfIdf();
+
+      try (PrintWriter writer = new PrintWriter("term-freqs.txt", "UTF-8")) {
+        for (Integer docId : justificationAnalyser.tfIdf.rowKeySet()) {
+          Map<String, Double> termFreqs = justificationAnalyser.tfIdf.row(docId);
+          for (String term : termFreqs.keySet()) {
+            writer.println(docId + "\t" + term + "\t" + termFreqs.get(term));
+          }
+        }
+      }
     }
   }
 }
